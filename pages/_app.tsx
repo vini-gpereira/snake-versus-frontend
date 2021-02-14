dc83a40b086c61ecd3814b98202a6e3d576a0645
viniciusgpereira <vini.gpereira2014@gmail.com>
2021-02-14 19:45:06 -0300

    chore: add title on header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import React, { FC } from 'react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import 'styles/globals.css';
 
 const App: FC<AppProps> = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Snake Versus</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
