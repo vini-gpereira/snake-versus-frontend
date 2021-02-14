68e1e426dc3ee4aade197e1bf4034a21601239e4
viniciusgpereira <vini.gpereira2014@gmail.com>
2021-02-14 16:20:32 -0300

    chore: setup global component file and paths on tsconfig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from "react";
-import type { AppProps } from "next/app";
-import "../styles/global.css";
+import React, { FC } from 'react';
+import type { AppProps } from 'next/app';
+import 'styles/globals.css';
 
 const App: FC<AppProps> = ({ Component, pageProps }: AppProps): JSX.Element => {
   return <Component {...pageProps} />;
