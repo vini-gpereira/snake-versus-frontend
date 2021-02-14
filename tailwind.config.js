4c924090dc7a19d19690e4987d93de860573bdba
viniciusgpereira <vini.gpereira2014@gmail.com>
2021-02-14 19:47:06 -0300

    chore: add default font and background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: '#7e846b',
+      secondary: '#0f0f0f',
+    },
+    fontFamily: {
+      default: ["'Press Start 2P'", 'cursive'],
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
