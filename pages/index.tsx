45e07e6e0bb9d98e9d55b9b8399cbe93ffc82a90
viniciusgpereira <vini.gpereira2014@gmail.com>
2021-02-14 23:32:45 -0300

    feat: create home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
-import React, { FC } from "react";
+import React, { FC } from 'react';
+import Image from 'next/image';
+import Button from 'components/button';
+import styles from 'styles/home.module.css';
 
 const Home: FC = (): JSX.Element => {
-  return <h1>Welcome to Snake Versus!</h1>;
+  return (
+    <div className={styles.wrapper}>
+      <main className={styles.menu}>
+        <h1>SnakeVersus</h1>
+        <nav>
+          <Button className={styles.option} href="/rooms/create" label="Play" />
+          <Button className={styles.option} href="/rooms" label="Join" />
+        </nav>
+      </main>
+      <a
+        className={styles['github-icon']}
+        href="https://github.com/vini-gpereira/snake-versus-frontend"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image src="/github-icon.png" height={60} width={60} />
+      </a>
+    </div>
+  );
 };
 
 export default Home;
