8e2bdf140af6fe3d968946e9879e8b09e3139ce5
root <root@DESKTOP-EB9OOC0.localdomain>
2021-02-22 21:12:40 -0300

    feat: enable button when inputs are filled

--- pages/rooms/index.tsx
+++ pages/rooms/index.tsx
@@ -1,21 +1,34 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
 import Button from 'components/button';
 import Layout from 'components/layout';
 import Input from 'components/input';
 import styles from 'styles/room-join.module.css';
 
 const RoomJoin: FC = (): JSX.Element => {
+  const [nickname, setNickname] = useState<string>('');
+  const [roomId, setRoomId] = useState<string>('');
+
   return (
     <Layout>
       <div className={styles.container}>
-        <Input className={styles.input} placeholder="Your nickname" />
+        <Input
+          className={styles.input}
+          placeholder="Your nickname"
+          value={nickname}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setNickname(e.target.value)}
+        />
         <div className={`bit-border ${styles.rooms}`}>
           <ul>
             <li></li>
           </ul>
         </div>
-        <Input className={styles.input} placeholder="Room code" />
-        <Button className={styles.button} action={() => false} label="Join" disabled={true} />
+        <Input
+          className={styles.input}
+          placeholder="Room code"
+          value={roomId}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setRoomId(e.target.value)}
+        />
+        <Button className={styles.button} action={() => false} label="Join" disabled={!nickname || !roomId} />
       </div>
     </Layout>
   );
