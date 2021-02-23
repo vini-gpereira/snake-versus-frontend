b591a94c009ccaa2c846253340aaa2f14653ef24
root <root@DESKTOP-EB9OOC0.localdomain>
2021-02-22 21:12:18 -0300

    feat: add input treatment on input component

--- components/input/index.tsx
+++ components/input/index.tsx
@@ -1,15 +1,17 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC } from 'react';
 import styles from './input.module.css';
 
 interface InputProps {
+  value?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
   className?: string;
   placeholder?: string;
 }
 
-const Input: FC<InputProps> = ({ className, placeholder }: InputProps): JSX.Element => {
+const Input: FC<InputProps> = ({ value, onChange, className, placeholder }: InputProps): JSX.Element => {
   return (
     <div className={`bit-border ${styles.wrapper} ${className}`}>
-      <input className={styles.input} type="text" placeholder={placeholder} />
+      <input className={styles.input} type="text" placeholder={placeholder} value={value} onChange={onChange} />
     </div>
   );
 };
